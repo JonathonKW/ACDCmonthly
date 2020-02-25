b7cca9163e300e7ccd05dd76a2be1e1d7d6302c4
JonathonKW <44815988+JonathonKW@users.noreply.github.com>
2020-02-25 12:08:35 -0500

    Added report generation feature

--- autoreporting.py
+++ autoreporting.py
@@ -10,3 +10,17 @@ env = Environment(
 )
 
 template = env.get_template("report.html")
+
+def main():
+    # Entry point for script
+    # Render a template, write to file
+    # :return:
+
+    with open("outputs/report.html", "w") as f:
+
+        # Here we link the template to our code
+        f.write(template.render(content=content))
+
+if __name__ == "__main__":
+    main()
+    
